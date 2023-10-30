78b64b292c5c77a3571b96613d9390c8c44b5123
mre55 <misganawmihreteab>
2023-10-30 15:55:47 +0300

    Render Result to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Result from "./components/Result";
 import UserInput from "./components/UserInput";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <UserInput />
+      <Result />
     </>
   );
 }
