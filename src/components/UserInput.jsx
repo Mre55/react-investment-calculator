32b25a4d356499157b849971f4f976fafac169c6
mre55 <misganawmihreteab>
2023-11-16 15:48:11 +0300

    Pass input fields using one object

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,24 +1,66 @@
-import React from "react";
+// UserInput.jsx
+import React, { useState } from "react";
 
-const UserInput = () => {
+const UserInput = ({ calculateInvestment }) => {
+  const [inputValues, setInputValues] = useState({
+    initialInvestment: "",
+    annualInvestment: "",
+    expectedReturn: "",
+    duration: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setInputValues({
+      ...inputValues,
+      [name]: value,
+    });
+
+    calculateInvestment(inputValues);
+  };
+
   return (
     <section id="user-input">
       <div className="input-group">
         <div>
           <label htmlFor="initial-investment">INITIAL INVESTMENT</label>
-          <input id="initial-investment" type="number" />
+          <input
+            id="initial-investment"
+            name="initialInvestment"
+            type="number"
+            value={inputValues.initialInvestment}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
-          <label htmlFor="anuual-investment">ANNUAL INVESTMENT</label>
-          <input id="anuual-investment" type="number" />
+          <label htmlFor="annual-investment">ANNUAL INVESTMENT</label>
+          <input
+            id="annual-investment"
+            name="annualInvestment"
+            type="number"
+            value={inputValues.annualInvestment}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
           <label htmlFor="expected-return">EXPECTED RETURN</label>
-          <input id="expected-return" type="number" />
+          <input
+            id="expected-return"
+            name="expectedReturn"
+            type="number"
+            value={inputValues.expectedReturn}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
           <label htmlFor="duration">DURATION</label>
-          <input id="duration" type="number" />
+          <input
+            id="duration"
+            name="duration"
+            type="number"
+            value={inputValues.duration}
+            onChange={handleInputChange}
+          />
         </div>
       </div>
     </section>
