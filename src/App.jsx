efd811907d7efff1884ccd34867ecf9357d6a799
mre55 <misganawmihreteab>
2023-11-16 15:48:26 +0300

    Recevie object from child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+// App.jsx
 import Header from "./components/Header";
 import Result from "./components/Result";
 import UserInput from "./components/UserInput";
+import { calculateInvestmentResults } from "./util/investment";
 
 function App() {
+  const handleCalculateInvestment = (inputValues) => {
+    const res = calculateInvestmentResults(inputValues);
+    console.log('res in App.js file is ', res)
+  };
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput calculateInvestment={handleCalculateInvestment} />
       <Result />
     </>
   );
