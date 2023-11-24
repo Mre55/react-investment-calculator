39b92bd96f47ff8cd063e5bf9a18314f451c11d5
mre55 <misganawmihreteab>
2023-11-24 12:39:40 +0300

    Add useState hook and pass result to Result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 // App.jsx
+import { useState } from "react";
 import Header from "./components/Header";
 import Result from "./components/Result";
 import UserInput from "./components/UserInput";
 import { calculateInvestmentResults } from "./util/investment";
 
 function App() {
+  const [result, setResult] = useState([])
+
   const handleCalculateInvestment = (inputValues) => {
     const res = calculateInvestmentResults(inputValues);
-    console.log('res in App.js file is ', res)
+    setResult(res)
   };
 
   return (
     <>
       <Header />
       <UserInput calculateInvestment={handleCalculateInvestment} />
-      <Result />
+      <Result result={result} />
     </>
   );
 }
