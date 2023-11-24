0ed464f95a72e3de53fcf35df97ae3b54b8e701b
mre55 <misganawmihreteab>
2023-11-24 13:34:57 +0300

    Refactor investment calculation in calculateInvestmentResults function

--- src/util/investment.js
+++ src/util/investment.js
@@ -8,23 +8,21 @@ export function calculateInvestmentResults({
   initialInvestment,
   annualInvestment,
   expectedReturn,
-  duration,
+  duration
 }) {
   const annualData = [];
   let investmentValue = initialInvestment;
 
   for (let i = 0; i < duration; i++) {
-    const interestEarnedInYear = investmentValue * expectedReturn;
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
     annualData.push({
       year: i + 1, // year identifier
       interest: interestEarnedInYear, // the amount of interest earned in this year
       valueEndOfYear: investmentValue, // investment value at end of year
-      annualInvestment: annualInvestment, // investment added in this year
+      annualInvestment: annualInvestment // investment added in this year
     });
   }
-
-  console.log('annual data in investment.js is ', annualData)
 
   return annualData;
 }
@@ -32,53 +30,9 @@ export function calculateInvestmentResults({
 // The browser-provided Intl API is used to prepare a formatter object
 // This object offers a "format()" method that can be used to format numbers as currency
 // Example Usage: formatter.format(1000) => yields "$1,000"
-export const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
+export const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
   minimumFractionDigits: 0,
-  maximumFractionDigits: 0,
+  maximumFractionDigits: 0
 });
-
-
-// // Input values
-// const initialInvestment = 10000;
-// const annualInvestment = 1200;
-// const expectedReturn = 0.06;
-// const duration = 10;
-
-// // Calculate investment results
-// const results = calculateInvestmentResults({
-//   initialInvestment,
-//   annualInvestment,
-//   expectedReturn,
-//   duration,
-// });
-
-// // Function to display the results for a specific year
-// function displayResultsForYear(year) {
-//   if (year <= results.length) {
-//     const result = results[year - 1];
-//     const curResult = results[year - 2];
-//     console.log(
-//       `Year ${year} | Investment Value = ${formatter.format(
-//         result.valueEndOfYear
-//       )} | Interest (Year) = ${formatter.format(
-//         result.interest
-//       )} | Total Interest = ${
-//         curResult
-//           ? formatter.format(result.interest + curResult.interest)
-//           : formatter.format(result.interest)
-//       } | Invested Capital = ${formatter.format(
-//         result.valueEndOfYear - result.interest
-//       )}`
-//     );
-//   } else {
-//     console.log(`Year ${year} | Data not available`);
-//   }
-// }
-
-// // Display the results for year 1
-// displayResultsForYear(1);
-
-// // Display the results for year 2
-// displayResultsForYear(2);
