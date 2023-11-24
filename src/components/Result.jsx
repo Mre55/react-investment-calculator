3a3fea39597f31426f0bb45f108baa74a36c8174
mre55 <misganawmihreteab>
2023-11-24 12:40:06 +0300

    Refactor Result component to render dynamic data

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-const Result = () => {
+const Result = ({ result }) => {
+  console.log("result is in result file ", result);
+
   return (
     <div id="result">
       <table>
@@ -13,43 +15,17 @@ const Result = () => {
             <th>Invested Capital</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>$10,850</td>
-            <td>$550</td>
-            <td>$550</td>
-            <td>$10,300</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>$11,747</td>
-            <td>$597</td>
-            <td>$1,147</td>
-            <td>$10,600</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>$12,693</td>
-            <td>$646</td>
-            <td>$1,147</td>
-            <td>$10,900</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>$13,691</td>
-            <td>$698</td>
-            <td>$2,491</td>
-            <td>$11,200</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>$14,744</td>
-            <td>$753</td>
-            <td>$3,244</td>
-            <td>$11,500</td>
-          </tr>
-        </tbody>
+        {result.map((res) => (
+          <tbody key={res.year}>
+            <tr>
+              <td>{res.year}</td>
+              <td>{res.annualInvestment}</td>
+              <td>{res.interest}</td>
+              <td>{res.valueEndOfYear}</td>
+              <td>{res.annualInvestment * res.interest}</td>
+            </tr>
+          </tbody>
+        ))}
       </table>
     </div>
   );
